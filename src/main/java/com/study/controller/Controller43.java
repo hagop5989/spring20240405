f40f613dabfeb354217f4def7f95ef371bc00a82
daeseong <zang5989@gmail.com>
2024-05-20 16:23:33 +0900

    20240520 react- 공부 RestController = Controller + ResponseBody (학원)

--- src/main/java/com/study/controller/Controller43.java
+++ src/main/java/com/study/controller/Controller43.java
@@ -6,22 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Random;
-
 @Controller
 @RequestMapping("/api/main43")
 public class Controller43 {
 
+    // ResponseEntity 상태코드를 직접 명시해서 응답할 때
     @GetMapping("sub1")
     @ResponseBody // ResponseEntity 응답일 땐 생략 가능
-    // ResponseEntity 상태코드를 직접 명시해서 응답할 때
     public ResponseEntity sub1() {
-        return ResponseEntity.status(200).build();
+
+//        return ResponseEntity.status(200).build();
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("sub2")
+//    @ResponseBody
     public ResponseEntity sub2() {
-        return ResponseEntity.status(400).build();
+
+//        return ResponseEntity.status(400).build();
+        return ResponseEntity.badRequest().build();
     }
 
     @GetMapping("sub3")
@@ -36,19 +39,13 @@ public class Controller43 {
 
     @GetMapping("sub5")
     public ResponseEntity sub5() {
-        return ResponseEntity.status(404).build();
+//        return ResponseEntity.status(404).build();
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping("sub6")
     public ResponseEntity sub6() {
-        return ResponseEntity.status(500).build();
-    }
-
-    @GetMapping("sub8")
-    public ResponseEntity sub8() {
-        int[] a = {200, 401, 403, 500};
-        int num = new Random().nextInt(a.length);
-        System.out.println("num = " + a[num]);
-        return ResponseEntity.status(a[num]).build();
+//        return ResponseEntity.status(500).build();
+        return ResponseEntity.internalServerError().build();
     }
 }
---
+++ src/main/java/com/study/controller/Controller45.java
@@ -0,0 +1,42 @@
+package com.study.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/main45")
+public class Controller45 {
+
+    @PostMapping("sub1")
+    public String sub1(@RequestParam("name") String name,
+                       @RequestParam("file") MultipartFile file) throws IOException {
+        System.out.println("name = " + name);
+        System.out.println("file.getOriginalFilename() = " + file.getOriginalFilename()); // 원 파일명 조회
+        System.out.println("file.getSize() = " + file.getSize());
+
+        String path = "C:/Temp/" + file.getOriginalFilename();
+        file.transferTo(new File(path)); // 파일 경로에 저장.
+        return null;
+    }
+
+    @PostMapping("sub2")
+    public String sub2(@RequestParam("name") String name,
+                       @RequestParam("file[]") MultipartFile[] file) throws IOException {
+        System.out.println("name = " + name);
+        if (file != null && file.length > 0) {
+            for (MultipartFile f : file) {
+                if (f.getSize() > 0) {
+                    String path = "C:/Temp/" + f.getOriginalFilename();
+                    f.transferTo(new File(path));
+                }
+            }
+        }
+        return null;
+    }
+}
